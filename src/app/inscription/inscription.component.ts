2699e4a48703ddcd1f7d5b1651faad5f1a6dfe19
erdmannremy <erdmannremy@gmail.com>
2023-12-01 13:19:30 +0100

    ProjetDevWeb/01-12/v1

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,34 +1,11 @@
-import { Component,OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
-export class InscriptionComponent {constructor(private http: HttpClient) { }
-
-ngOnInit() { }
-
-onSubmit() {
-  // Récupère les données d'inscription du formulaire
-  const data = {
-    email: this.emailInput.value,
-    password: this.passwordInput.value,
-  };
-
-  // Soumet les données d'inscription au serveur
-  this.http.post('/api/inscription', data)
-    .subscribe((response) => {
-      if (response.status === 200) {
-        // L'inscription a réussi
-        this.router.navigate(['/compte']);
-      } else {
-        // L'inscription a échoué
-        alert('Erreur lors de l\'inscription');
-      }
-    });
-}
+export class InscriptionComponent {
+  
 
 }
-
-
