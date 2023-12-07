878380778dc8cb0880855adea2d1f6d953befd9d
erdmannremy <erdmannremy@gmail.com>
2023-12-07 11:40:19 +0100

    ProjetDevWeb/07-12/v2

--- src/app/Users/users-register/usersregister.component.spec.ts
+++ src/app/Users/users-register/usersregister.component.spec.ts
@@ -2,6 +2,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UsersregisterComponent } from './usersregister.component';
 
+import { FormControl,Validators } from '@angular/forms';
+
+const monchamp = new FormControl('',[
+  Validators.required,      //verifier si le champ est vide
+  Validators.minLength(5),  //vérifier la longueur minimale de la chaine
+  Validators.email,         //vérifier si la valeur est une adresse e-mail valide
+          
+               //ajoutez d'autres validateurs au besoin
+  ]);
+               
+const emailControl = new FormControl('',[
+  Validators.required,      // Le champ est requis
+  Validators.email,         // la valeur doit être une adresse e-mail valide 
+
+             //ajoutez d'autres validateurs au besoin
+
+]);
+
 describe('UsersregisterComponent', () => {
   let component: UsersregisterComponent;
   let fixture: ComponentFixture<UsersregisterComponent>;
--- src/app/Users/users-register/usersregister.component.ts
+++ src/app/Users/users-register/usersregister.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder,FormGroup,Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usersregister',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./usersregister.component.css']
 })
 export class UsersregisterComponent {
+  usersregister:FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.usersregister = this.fb.group({
+      firstName:['',Validators.required],
+      lastName :['',Validators.required],
+      birthDate:['',Validators.required],
+      country:['',Validators.required],
+      city:['',Validators.required],
+      adress:['',Validators.required],
+      adressNumber:['',Validators.required],
+      codepostal:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
+      phoneNumber:['',Validators.required],
+    });
+  }
 
 }
